3d26ee9d5c5ed38a3c470041efd5a1d0a06add58
Ulaskhan <ulan0505ulan@gmail.com>
2026-04-25 00:44:41 +0500

    structural changes

--- bookmate-frontend/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from 'expo-router';
-import { useStore } from '../hooks/useStore';
-
-export default function Index() {
-  const token = useStore((s) => s.token);
-  if (token) return <Redirect href="/(tabs)" />;
-  return <Redirect href="/auth/login" />;
-}
